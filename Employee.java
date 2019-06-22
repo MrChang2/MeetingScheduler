58e3290e3e4989ea6f971fe891061f1cdd5d2cd4
MrChang2 <43765332+MrChang2@users.noreply.github.com>
2019-06-22 16:40:33 -0700

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,60 @@
+public class Employee {
+    private String employeeID;
+    private String first_name;
+    private String last_name;
+    private String username;
+    private String password;
+    private String position;
+
+    public Employee() {
+
+    }
+
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+}
