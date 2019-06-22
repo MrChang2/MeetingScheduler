c2ab077dd7e60835fd4f51290809d37b0b3c5174
MrChang2 <43765332+MrChang2@users.noreply.github.com>
2019-06-22 16:41:34 -0700

    Create Meeting.java

---
+++ Meeting.java
@@ -0,0 +1,26 @@
+public class Meeting {
+    Room room;
+    Employee[] attendees;
+
+    public Meeting() {
+
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void invite() {
+
+    }
+
+    public Employee getAttendee() {
+        Employee e = new Employee();
+
+        return e;
+    }
+}
