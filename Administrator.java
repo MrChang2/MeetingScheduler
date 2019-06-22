a4b4f3860ab908433a58e6e86b6ee403892ff9d4
MrChang2 <43765332+MrChang2@users.noreply.github.com>
2019-06-22 16:42:10 -0700

    Create Administrator.java

---
+++ Administrator.java
@@ -0,0 +1,24 @@
+public class Administrator {
+    public Administrator() {
+
+    }
+
+    public void addEmployee() {
+
+    }
+    public void deleteEmployee() {
+
+    }
+    public void updateEmployee() {
+
+    }
+    public void addRoom() {
+
+    }
+    public void deleteRoom() {
+
+    }
+    public Employee getEmployee() {
+
+    }
+}
