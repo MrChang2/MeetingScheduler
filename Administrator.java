4051f595e85e43e77a11fd5c22b479edb71fec0c
MrChang2 <43765332+MrChang2@users.noreply.github.com>
2019-06-22 18:18:42 -0700

    Update Administrator.java

--- Administrator.java
+++ Administrator.java
@@ -1,24 +1,28 @@
+import java.util.ArrayList;
 public class Administrator {
-    public Administrator() {
-
+    private Employee_Database employeeDatabase;
+    private Room_Database roomDatabase;
+    public Administrator(Employee_Database d) {
+        employeeDatabase = d;
     }
 
-    public void addEmployee() {
-
+    public void addEmployee(Employee e) {
+        employeeDatabase.getEmployeeDatabase().add(e);
     }
-    public void deleteEmployee() {
-
+    public void deleteEmployee(Employee e) {
+        employeeDatabase.getEmployeeDatabase().remove(e);
     }
     public void updateEmployee() {
 
     }
-    public void addRoom() {
-
+    public void addRoom(Room r) {
+        roomDatabase.getRooms().add(r);
     }
-    public void deleteRoom() {
-
+    public void deleteRoom(Room r) {
+        roomDatabase.getRooms().remove(r);
     }
-    public Employee getEmployee() {
-
+    public Employee getEmployee(Employee e) {
+        ArrayList<Employee> database = employeeDatabase.getEmployeeDatabase();
+        return database.get(database.indexOf(e));
     }
 }
