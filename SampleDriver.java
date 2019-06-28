6b83aec4acc75a7ed9678ca23fd3014e8d95f273
MrChang2 <43765332+MrChang2@users.noreply.github.com>
2019-06-28 15:38:50 -0700

    Update SampleDriver.java

--- SampleDriver.java
+++ SampleDriver.java
@@ -2,32 +2,36 @@ public class SampleDriver {
     public static void main(String[] args) {
         Employee_Database employees = new Employee_Database();
         Room_Database rooms = new Room_Database();
-        Administrator admin = new Administrator(employees, rooms);
+        Meeting_Database meetings = new Meeting_Database();
 
-        Employee e1 = new Employee("001", "Bob", "Hanson", "BobTheBuilder09", "wecanfixit", "Contractor");
-        Employee e2 = new Employee("032", "John", "Smith", "BabaYaga", "iamjohnwick", "Manager");
-        Employee e3 = new Employee("529", "Selina", "Kent", "Supercat", "totallylegit", "Secretary");
+        Employee e1 = new Employee("3190", "Steve", "Blum", "stevetheking", "iamtheking", "Assistant Manager", "9:00 am", "5:00 pm");
+        Administrator e2 = new Administrator("4512", "Marty", "Stewart", "martystu", "getsmart", "Manager", "8:00 am", "6:00 pm");
+        e2.setEmployeeDatabase(employees);
+        e2.setRoomDatabase(rooms);
 
-        Room r1 = new Room("11034");
-        Room r2 = new Room("32847");
-        Room r3 = new Room("64235");
+        employees.getEmployeeDatabase().add(e1);
+        employees.getEmployeeDatabase().add(e2);
 
-        admin.addEmployee(e1);
-        admin.addEmployee(e2);
-        admin.addEmployee(e3);
+        Login newLogin = new Login(employees);
+        Employee User = newLogin.LoginCheck();
+        if (User!=null) {
+            System.out.println("Welcome " + User.getFirst_name() + "!");
+        }
 
-        admin.addRoom(r1);
-        admin.addRoom(r2);
-        admin.addRoom(r3);
+        Room r1 = new Room("101");
+        Room r2 = new Room("234");
+        Room r3 = new Room("815");
 
-        admin.viewAllEmployeeInfo();
-        admin.viewAllRoomInfo();
+        rooms.getRooms().add(r1);
+        rooms.getRooms().add(r2);
+        rooms.getRooms().add(r3);
 
-        System.out.println("Removing Bob and first room:");
-        admin.deleteEmployee(e1);
-        admin.deleteRoom(r1);
+        User.createMeeting(meetings, rooms);
+        System.out.println("You have created a new meeting.");
+        System.out.println("Owner ID: " + meetings.getMeetingDatabase().get(0).getOwnerID());
+        System.out.println("Room ID: " + meetings.getMeetingDatabase().get(0).getRoom().getRoomID());
 
-        admin.viewAllEmployeeInfo();
-        admin.viewAllRoomInfo();
+        User.inviteEmployee(employees, meetings);
+        User.inviteEmployee(employees, meetings);
     }
 }
